fc1e9c8a7a8e01e65890d2f71ec2e4f2883d4d2a
Mankatico <anderssonbustos@gmail.com>
2026-06-03 17:19:43 -0500

    Chore: initialize project

---
+++ src/main/java/com/academicmanagement/academic_management_api/AcademicManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.academicmanagement.academic_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academicmanagement/academic_management_api/AcademicManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academicmanagement.academic_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
